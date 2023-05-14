72d638f401b5f845bd4516bd607031cbe1cb03db
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-05-15 01:01:29 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 import { Cart } from './components/Cart/Cart';
-import { Navigation } from './components/Navigation/Navigation'
-import { Home } from './pages/Home'
+import { Navigation } from './components/Navigation/Navigation';
+import { Home } from './pages/Home';
 import { Liked } from './pages/Liked';
 import { Orders } from './pages/Orders';
 import { Routes, Route, Link } from 'react-router-dom';
@@ -11,164 +11,204 @@ import { Routes, Route, Link } from 'react-router-dom';
 import styles from './App.module.css';
 
 export function App() {
-  const [items, setItems] = React.useState([])
-  const [isLoading, setIsLoading] = React.useState(true)
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
-    async function fetchData(){
+    async function fetchData() {
       try {
-        setIsLoading(true)
-        const itemsResponse = await axios.get("https://63fce95c859df29986c75869.mockapi.io/items")
-        const cartItemsResponse = await axios.get("https://63fce95c859df29986c75869.mockapi.io/cart")
-        const likedItemsResponse = await axios.get("https://64248fef7ac292e3cfed5b72.mockapi.io/Liked_items")
-        
-        setIsLoading(false)
-        setCartItems(cartItemsResponse.data)
-        setLikedItems(likedItemsResponse.data)
-        setItems(itemsResponse.data)
-      }
-      catch(error){
-        alert("Error when getting data from db on first render")
-        console.log(error)
+        setIsLoading(true);
+        const itemsResponse = await axios.get('https://63fce95c859df29986c75869.mockapi.io/items');
+        const cartItemsResponse = await axios.get(
+          'https://63fce95c859df29986c75869.mockapi.io/cart',
+        );
+        const likedItemsResponse = await axios.get(
+          'https://64248fef7ac292e3cfed5b72.mockapi.io/Liked_items',
+        );
+
+        setIsLoading(false);
+        setCartItems(cartItemsResponse.data);
+        setLikedItems(likedItemsResponse.data);
+        setItems(itemsResponse.data);
+      } catch (error) {
+        alert('Error when getting data from db on first render');
+        console.log(error);
       }
     }
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
-  const [cartItems, setCartItems] = React.useState([])
+  const [cartItems, setCartItems] = React.useState([]);
 
-  async function onAddToCart(obj){
-    try{
-      const findItem = cartItems.find(item => Number(item.parentId) === Number(obj.id))
-      if (!findItem){
-        await axios.post("https://63fce95c859df29986c75869.mockapi.io/cart", obj)
-        setCartItems(prev => [...prev, obj])
+  async function onAddToCart(obj) {
+    try {
+      const findItem = cartItems.find((item) => Number(item.parentId) === Number(obj.id));
+      if (!findItem) {
+        await axios.post('https://63fce95c859df29986c75869.mockapi.io/cart', obj);
+        setCartItems((prev) => [...prev, obj]);
       }
-    }
-    catch(error){
-      alert("Error on adding item to cart")
-      console.log(error)
-    }
-  }
-
-  async function onRemoveFromCart(obj){
-    const {data} = await axios.get("https://63fce95c859df29986c75869.mockapi.io/cart")
-    const deletedItem = data.find(item => Number(item.parentId) === Number(obj.parentId))
-    try{
-      if (deletedItem){
-        setCartItems(prev => prev.filter(item => Number(item.parentId) !== Number(obj.parentId)))
-        await axios.delete(`https://63fce95c859df29986c75869.mockapi.io/cart/${deletedItem.id}`)
-      }
-    }
-    catch(error){
-      alert("Error when removing item from cart")
-      console.log(error)
+    } catch (error) {
+      alert('Error on adding item to cart');
+      console.log(error);
     }
   }
 
-  const [isCartOpened, setCart] = React.useState(false)
-
-  function changeCart(){
-    setCart(isCartOpened=>!isCartOpened)
+  async function onRemoveFromCart(obj) {
+    const { data } = await axios.get('https://63fce95c859df29986c75869.mockapi.io/cart');
+    const deletedItem = data.find((item) => Number(item.parentId) === Number(obj.parentId));
+    try {
+      if (deletedItem) {
+        setCartItems((prev) =>
+          prev.filter((item) => Number(item.parentId) !== Number(obj.parentId)),
+        );
+        await axios.delete(`https://63fce95c859df29986c75869.mockapi.io/cart/${deletedItem.id}`);
+      }
+    } catch (error) {
+      alert('Error when removing item from cart');
+      console.log(error);
+    }
   }
 
-  const [searchInput, setSearchInput] = React.useState("")
-  function onChangeSearchInput(event){
-    setSearchInput(event.target.value)
+  const [isCartOpened, setCart] = React.useState(false);
+
+  function changeCart() {
+    setCart((isCartOpened) => !isCartOpened);
   }
 
-  const [likedItems, setLikedItems] = React.useState([])
-    const onAddToLiked = async (likedObj) => {
-      const likedItem = likedItems.find(obj => obj.parentId === likedObj.parentId)
-      try{
-        if (likedItem){
-          axios.delete(`https://64248fef7ac292e3cfed5b72.mockapi.io/Liked_items/${likedItem.id}`)
-          setLikedItems(prev => prev.filter(item => item.parentId !== likedObj.parentId))
-        }
-        else{
-          const {data} = await axios.post("https://64248fef7ac292e3cfed5b72.mockapi.io/Liked_items", likedObj)
-          setLikedItems(prev => [...prev, data])
-        }
-      }
-      catch(error){
-        alert("Error with sending object to backend in onAddToLiked function")
-        console.log(error)
-      }
-      console.log(likedItems)
-      }
+  const [searchInput, setSearchInput] = React.useState('');
+  function onChangeSearchInput(event) {
+    setSearchInput(event.target.value);
+  }
 
-  const [isOrdered, setIsOrdered] = React.useState(false)
-  const [orderId, setOrderId] = React.useState(null)
-  const [isOrderLoading, setOrderLoading] = React.useState(false)
+  const [likedItems, setLikedItems] = React.useState([]);
+  const onAddToLiked = async (likedObj) => {
+    const likedItem = likedItems.find((obj) => obj.parentId === likedObj.parentId);
+    try {
+      if (likedItem) {
+        axios.delete(`https://64248fef7ac292e3cfed5b72.mockapi.io/Liked_items/${likedItem.id}`);
+        setLikedItems((prev) => prev.filter((item) => item.parentId !== likedObj.parentId));
+      } else {
+        const { data } = await axios.post(
+          'https://64248fef7ac292e3cfed5b72.mockapi.io/Liked_items',
+          likedObj,
+        );
+        setLikedItems((prev) => [...prev, data]);
+      }
+    } catch (error) {
+      alert('Error with sending object to backend in onAddToLiked function');
+      console.log(error);
+    }
+    console.log(likedItems);
+  };
 
-  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+  const [isOrdered, setIsOrdered] = React.useState(false);
+  const [orderId, setOrderId] = React.useState(null);
+  const [isOrderLoading, setOrderLoading] = React.useState(false);
 
-  async function makeOrder(){
-    try{
-      setOrderLoading(true)
-      const {data} = await axios.post("https://64248fef7ac292e3cfed5b72.mockapi.io/Orders", {items: cartItems})
-      setOrderId(data.id)
-      setIsOrdered(true)
-      setCartItems([])
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  async function makeOrder() {
+    try {
+      setOrderLoading(true);
+      const { data } = await axios.post('https://64248fef7ac292e3cfed5b72.mockapi.io/Orders', {
+        items: cartItems,
+      });
+      setOrderId(data.id);
+      setIsOrdered(true);
+      setCartItems([]);
       for (let index = 0; index < cartItems.length; index++) {
-        const item = cartItems[index]
-        await axios.delete(`https://63fce95c859df29986c75869.mockapi.io/cart/${item.id}`)
-        await delay(1000)
+        const item = cartItems[index];
+        await axios.delete(`https://63fce95c859df29986c75869.mockapi.io/cart/${item.id}`);
+        await delay(1000);
       }
-      setOrderLoading(false)
-    }
-    catch(error){
-      alert("Error in makeOrder function")
-      console.log(error)
+      setOrderLoading(false);
+    } catch (error) {
+      alert('Error in makeOrder function');
+      console.log(error);
     }
   }
 
-  const [isMenuClicked, setMenuClick] = React.useState(false)
+  const [isMenuClicked, setMenuClick] = React.useState(false);
 
   return (
     <div className={styles.main_body}>
-      
       <div className={styles.logo}>
-        <Link to='/'>
+        <Link to="/">
           <img alt="logo" src="img/icons/logo-template.svg"></img>
         </Link>
       </div>
-      
 
       <nav className={styles.navigation}>
-            <div className={styles.search}>
-              <img className={styles.magnifying_glass} alt="magnifying-glass" src="img/icons/magnifying-glass.svg"></img>
-              <input onChange={onChangeSearchInput} type="text" placeholder='SEARCH'></input>
-            </div>
-            <Link to='/orders'>
-              <div className={styles.icons}><img alt="user" src="img/icons/user.svg"></img></div>
-            </Link>
-            <Link to='/liked'>
-              <div className={styles.icons}><img alt="liked-items" src="img/icons/heart-fill.svg"></img></div>
-            </Link>
-            <div className={styles.icons} onClick={changeCart}><img alt="bag" src="img/icons/bag-outline.svg"></img></div>
-        </nav>
-        
-        <nav onMouseEnter={() => setMenuClick(true)} className={styles.hamburger_menu}>
-            <img alt="menu" src="img/icons/hamburger-menu.svg"></img>
-        </nav>
+        <div className={styles.search}>
+          <img
+            className={styles.magnifying_glass}
+            alt="magnifying-glass"
+            src="/img/icons/magnifying-glass.svg"></img>
+          <input onChange={onChangeSearchInput} type="text" placeholder="SEARCH"></input>
+        </div>
+        <Link to="/orders">
+          <div className={styles.icons}>
+            <img alt="user" src="/img/icons/user.svg"></img>
+          </div>
+        </Link>
+        <Link to="/liked">
+          <div className={styles.icons}>
+            <img alt="liked-items" src="img/icons/heart-fill.svg"></img>
+          </div>
+        </Link>
+        <div className={styles.icons} onClick={changeCart}>
+          <img alt="bag" src="img/icons/bag-outline.svg"></img>
+        </div>
+      </nav>
 
-        {isMenuClicked && <Navigation setMenuClick={setMenuClick}></Navigation>}
-        <Cart setIsOrdered={setIsOrdered} isOrderLoading={isOrderLoading} orderId={orderId} isOrdered={isOrdered} makeOrder={makeOrder} cart={cartItems} close={changeCart} onRemove={onRemoveFromCart} isOpened={isCartOpened}></Cart>
+      <nav onMouseEnter={() => setMenuClick(true)} className={styles.hamburger_menu}>
+        <img alt="menu" src="img/icons/hamburger-menu.svg"></img>
+      </nav>
 
-        <Routes>
-          <Route path='/' exact element={
-            <Home searchInput={searchInput} items={items} cartItems={cartItems} onAddToCart={onAddToCart} onAddToLiked={onAddToLiked} likedItems={likedItems}
-            isLoading={isLoading} />}>
-          </Route>
+      {isMenuClicked && <Navigation setMenuClick={setMenuClick}></Navigation>}
+      <Cart
+        setIsOrdered={setIsOrdered}
+        isOrderLoading={isOrderLoading}
+        orderId={orderId}
+        isOrdered={isOrdered}
+        makeOrder={makeOrder}
+        cart={cartItems}
+        close={changeCart}
+        onRemove={onRemoveFromCart}
+        isOpened={isCartOpened}></Cart>
 
-          <Route path='/liked' exact element={
-            <Liked searchInput={searchInput} cartItems={cartItems} onAddToCart={onAddToCart} items={likedItems} onAddToLiked={onAddToLiked}/>}>
-          </Route>
+      <Routes>
+        <Route
+          path="/"
+          exact
+          element={
+            <Home
+              searchInput={searchInput}
+              items={items}
+              cartItems={cartItems}
+              onAddToCart={onAddToCart}
+              onAddToLiked={onAddToLiked}
+              likedItems={likedItems}
+              isLoading={isLoading}
+            />
+          }></Route>
 
-          <Route path='/orders' exact element={<Orders searchInput={searchInput}></Orders>}></Route>
-        </Routes>
+        <Route
+          path="/liked"
+          exact
+          element={
+            <Liked
+              searchInput={searchInput}
+              cartItems={cartItems}
+              onAddToCart={onAddToCart}
+              items={likedItems}
+              onAddToLiked={onAddToLiked}
+            />
+          }></Route>
+
+        <Route path="/orders" exact element={<Orders searchInput={searchInput}></Orders>}></Route>
+      </Routes>
     </div>
-  )
+  );
 }
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,54 +1,56 @@
-import React from "react"
-import axios from "axios"
+import React from 'react';
+import axios from 'axios';
 
-import { Card } from "../components/Card/Card"
+import { Card } from '../components/Card/Card';
 
-import styles from "../App.module.css"
+import styles from '../App.module.css';
 
-export function Orders({searchInput}){
+export function Orders({ searchInput }) {
+  const [items, setItems] = React.useState([]);
+  // const [isLoading, setIsLoading] = React.useState(false)
 
-    const [items, setItems] = React.useState([])
-    const [isLoading, setIsLoading] = React.useState(false)
+  React.useEffect(() => {
+    try {
+      (async () => {
+        // setIsLoading(true)
+        const { data } = await axios.get('https://64248fef7ac292e3cfed5b72.mockapi.io/Orders');
+        setItems(data.map((obj) => obj.items));
+        // setIsLoading(false)
+      })();
+    } catch (error) {
+      alert('Error with getting orders!!!');
+      console.log(error);
+    }
+  }, []);
 
-    React.useEffect(() => {
-        try{
-            (async () => {
-                setIsLoading(true)
-                const {data} = await axios.get("https://64248fef7ac292e3cfed5b72.mockapi.io/Orders")
-                setItems(data.map(obj => obj.items))
-                setIsLoading(false)
-            })()
-        }
-        catch(error){
-            alert("Error with getting orders!!!")
-            console.log(error)
-        }
-    }, [])
-
-    return(
-        <>
-        <div className={styles.current_category}>
-            <div>
-                {searchInput ? <div className={styles.bolder}>Search by: {searchInput}</div>: <span className={styles.bolder}>My Orders</span>}
-            </div>
-
-            <div className={styles.filter_buttons}>
-                <button className={styles.see}>See 2</button>
-                <button className={styles.filter}>Filter</button>
-            </div>
+  return (
+    <>
+      <div className={styles.current_category}>
+        <div>
+          {searchInput ? (
+            <div className={styles.bolder}>Search by: {searchInput}</div>
+          ) : (
+            <span className={styles.bolder}>My Orders</span>
+          )}
         </div>
 
-            {items.map((obj, index) => (
-                <>
-                <p className={styles.order_num}>Order #{index+1}</p>
-                <br></br>
-                <main className={styles.main_grid}>
-                    {obj.map(item => (
-                        <Card {...item}></Card>
-                    ))}
-                </main>
-                </>
+        <div className={styles.filter_buttons}>
+          <button className={styles.see}>See 2</button>
+          <button className={styles.filter}>Filter</button>
+        </div>
+      </div>
+
+      {items.map((obj, index) => (
+        <>
+          <p className={styles.order_num}>Order #{index + 1}</p>
+          <br></br>
+          <main className={styles.main_grid}>
+            {obj.map((item) => (
+              <Card {...item}></Card>
             ))}
+          </main>
         </>
-    )
+      ))}
+    </>
+  );
 }
